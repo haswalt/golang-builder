1cb7960d3056dfee74e624351f3a2ee82b688aac
Harry Walter <harry.walter@lqdinternet.com>
2017-03-21 10:44:05 +0000

    Not maintained

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-source /build_environment.sh
-
-
-echo "Building $pkgName"
-`CGO_ENABLED=${CGO_ENABLED:-0} $builder build -a --installsuffix cgo --ldflags="${LDFLAGS:--s}" $pkgName`
